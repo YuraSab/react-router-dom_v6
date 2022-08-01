3beadf39e4bba4a528229dc692aa2e058e281629
Yura Sabat <yrasabat@gmail.com>
2022-08-01 10:56:13 +0300

    HeaderAndRoutes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Blog} from "./pages/Blog";
+import {Link, Route, Routes} from "react-router-dom";
+import {NotFound} from "./pages/NotFound";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <div className={'header'}>
+                <Link className={'link'} to={'/'}>
+                    <div className={'linkDiv'}>
+                        Home
+                    </div>
+                </Link>
+
+                <Link className={'link'} to={'/about'}>
+                    <div className={'linkDiv'}>
+                        About
+                    </div>
+                </Link>
+                <Link className={'link'} to={'/blog'}>
+                    <div className={'linkDiv'}>
+                        Blog
+                    </div>
+                </Link>
+            </div>
+
+            <div className={'routes'}>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/about'} element={<About/>}/>
+                    <Route path={'/blog'} element={<Blog/>}/>
+                    <Route path={'*'} element={<NotFound/>}/>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+        <div>
+            <h1>
+                About page
+            </h1>
+        </div>
+    )
+}
+
+export {About}
